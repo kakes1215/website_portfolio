bf3477738cb0f92262753e886599ab028cbadfda
Kaylynn <kdiazs1215@gmail.com>
2021-10-20 17:23:02 -0500

    Added Projects Section

--- website_portfolio/src/components/Projects.js
+++ website_portfolio/src/components/Projects.js
@@ -1,9 +1,43 @@
 import React from 'react';
-import { Pane, Heading } from 'evergreen-ui'
+import { Pane, Heading, Text } from 'evergreen-ui'
 
 function Project() {
     return (
         <>
+            <Heading marginTop={150} marginBottom={25} size="800">
+                Projects
+            </Heading>
+            <Pane>
+                <Pane
+                    display="flex"
+                    flexDirection="column"
+                    float="left"
+                    marginLeft={200}
+                    marginRight={200}
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Heading size="800">
+                        Project Title
+                    </Heading>
+                    <Text>
+                        Summary
+                    </Text>
+                </Pane>
+                <Pane
+                    display="flex"
+                    flexDirection="column"
+                    float="left"
+                    marginLeft={200}
+                    marginRight={200}
+                    justifyContent="center"
+                    alignItems="center"
+
+                >
+                    <Heading>Embed Project Here</Heading>
+
+                </Pane>
+            </Pane>
         </>
     )
 }
