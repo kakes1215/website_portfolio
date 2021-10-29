1defe6725653b766f8ef652f2a991f28b0d93316
Kaylynn <kdiazs1215@gmail.com>
2021-10-29 16:57:12 -0500

    Added greeting page

--- website_portfolio/src/components/App.js
+++ website_portfolio/src/components/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Greeting />
-      <NavBar />
+      {/* <NavBar /> */}
       <AboutMe />
       <Project />
       <Skills />
--- website_portfolio/src/components/Greeting.js
+++ website_portfolio/src/components/Greeting.js
@@ -1,19 +1,45 @@
-import { Pane, Heading } from 'evergreen-ui'
-import '../css/Greeting.css';
-
-
+import React, { } from 'react';
+import { grommet } from 'grommet/themes';
+import { Grommet, Paragraph, Box, Text, Grid } from 'grommet';
+import { Down } from 'grommet-icons';
 
 function Greeting() {
     return (
-        <>
+        <Grommet theme={grommet} >
+            <Box background={{ color: 'neutral-2' }} width='70%'>
+                <Grid
+                    rows={['auto', 'flex', 'flex', 'auto']}
+                    columns={['auto']}
+                    areas={[
+                        ['helloText'],
+                        ['nameText'],
+                        ['introParagraph'],
+                        ['downArrow']
+                    ]}
+                    pad='large'
+                >
+                    <Box gridArea='helloText' align="start">
+                        <Text size="xlarge" color="accent-1">Hello, my name is</Text>
+                    </Box>
+                    <Box gridArea='nameText' align="start" >
+                        <Text size="5xl" color='accent-2'>Kaylynn</Text>
+                        <Text size="5xl" color='#FFBCFF'>I develop web applications.</Text>
+                    </Box>
+                    <Box gridArea='introParagraph' align="start">
+                        <Paragraph textAlign='start' color='accent-1'>
+                            I'm a aspiring Software Engineer that specializes in Front End and Full Stack Development for web applications.
+                        </Paragraph>
+                    </Box>
+                    {/* <Box gridArea='downArrow' animation={{ type: 'pulse', duration: 1000 }} align='center' >
+                        <Down color='accent-1' size='large' />
+                        <Down color='accent-1' size='large' />
+                    </Box> */}
+                </Grid>
 
-            <Pane padding={300}>
-                <Heading size="900">
-                    HELLO THERE
-                </Heading>
-            </Pane>
-        </>
+            </Box>
+        </Grommet >
 
     )
+
 }
 export default Greeting
